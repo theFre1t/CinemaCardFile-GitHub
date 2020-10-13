7d723493c75a86a4625475b6683b3d2656500721
tFre1t <prost.tozan@yandex.ru>
2020-10-14 00:50:22 +0300

    CinemaLib Started

---
+++ CinemaCardFile/CinemaCardFile/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CinemaCardFile.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
